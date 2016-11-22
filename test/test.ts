7f104f4d20d9f2518240ebeb95100cd0a873c603
Megan Schoendorf <megan.schoendorf@gmail.com>
2016-11-22 15:51:21 -0800

    added tests

---
+++ test/test.ts
@@ -0,0 +1,46 @@
+/**
+ * Created by meganschoendorf on 11/22/16.
+ */
+import { assert } from 'chai'
+import { timed, timedAsync } from "../index";
+
+let className: string;
+let functionName: string;
+let duration: number;
+
+class TestClass {
+    @timedAsync(timedFunction)
+    async foo() {
+
+    }
+
+    @timed(timedFunction)
+    foo2() {
+
+    }
+}
+
+async function timedFunction(a: string, b: string, c: number) {
+    className = a;
+    functionName = b;
+    duration = c;
+}
+
+describe("test", function () {
+    it("timed async", async function () {
+        const testClass = new TestClass();
+        await testClass.foo();
+        assert.equal(className, 'TestClass');
+        assert.equal(functionName, 'foo');
+        assert.approximately(duration, 2, 2);
+    });
+
+    it("timed", async function () {
+        const testClass = new TestClass();
+        await testClass.foo2();
+        assert.equal(className, 'TestClass');
+        assert.equal(functionName, 'foo2');
+        assert.approximately(duration, 2, 2);
+    });
+
+});
