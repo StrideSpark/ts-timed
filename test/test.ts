a3b1681163968b5038d8bd1f5ce12d9d7f83edfe
mattnaides <matt.naides@gmail.com>
2018-02-02 13:31:34 -0800

    more test cases

--- test/test.ts
+++ test/test.ts
@@ -2,15 +2,21 @@
  * Created by meganschoendorf on 11/22/16.
  */
 import { assert } from 'chai';
-import { timed, timedAsync } from '../src/index';
+import { timed, timedAsync, timedAsyncWithTags } from '../src/index';
 
 let className: string;
 let functionName: string;
 let duration: number;
+let tags: Object;
 
 class TestClass {
     @timedAsync(timedFunction)
     async foo() {
+        return 3;
+    }
+
+    @timedAsyncWithTags(timedFunctionWithTags, { category: 'cat' })
+    async fooTags() {
         return 3;
     }
 
@@ -30,12 +36,29 @@ class ErrorClass {
     bar() {
         return 'bar';
     }
+
+    @timed(5 as any)
+    notAFunc() {
+        return 'notAFunc';
+    }
+
+    @timedAsync(undefined as any)
+    async notAFuncAsync() {
+        return 'notAFuncAsync';
+    }
 }
 
 async function timedFunction(a: string, b: string, c: number) {
     className = a;
     functionName = b;
     duration = c;
+}
+
+async function timedFunctionWithTags(a: string, b: string, c: number, tag: object) {
+    className = a;
+    functionName = b;
+    duration = c;
+    tags = tag;
 }
 
 async function errorFunction(_a: string, _b: string, _c: number) {
@@ -51,6 +74,15 @@ describe('test', function() {
         assert.approximately(duration, 2, 2);
     });
 
+    it('timed async with tags', async function() {
+        const testClass = new TestClass();
+        assert.equal(await testClass.fooTags(), 3);
+        assert.equal(className, 'TestClass');
+        assert.equal(functionName, 'fooTags');
+        assert.approximately(duration, 2, 2);
+        assert.deepEqual(tags, { category: 'cat' });
+    });
+
     it('timed', function() {
         const testClass = new TestClass();
         assert.equal(testClass.foo2(), 'foo2');
@@ -61,11 +93,15 @@ describe('test', function() {
 
     it('timed async errors', async function() {
         const errorClass = new ErrorClass();
+        errorClass.foo.apply({}, {});
+        errorClass.notAFuncAsync.apply({}, {});
         assert.equal(await errorClass.foo(), 5);
+        assert.equal(await errorClass.notAFuncAsync(), 'notAFuncAsync');
     });
 
     it('timed errors', function() {
         const errorClass = new ErrorClass();
         assert.equal(errorClass.bar(), 'bar');
+        assert.equal(errorClass.notAFunc(), 'notAFunc');
     });
 });
